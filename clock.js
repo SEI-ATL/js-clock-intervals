6461b3fb7d63ff9d8aa26628befebc6b940f6e16
MusicMan <musicman@MacBook-Pro.fios-router.home>
2020-10-23 12:22:35 -0400

    [js-clock-intervals]Add 1st Stage Clock

---
+++ clock.js
@@ -0,0 +1,5 @@
+const hour = document.querySelector('#hour')
+const minute = document.querySelector('#minute')
+const second = document.querySelector('second')
+const face = document.querySelector('face')
+
